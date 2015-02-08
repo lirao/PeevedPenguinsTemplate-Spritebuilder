8652963fafd133ee87c07fe9f71b92840e10a0b8
Rao Li <rao.li@sv.cmu.edu>
2015-02-08 14:47:49 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,6 +13,7 @@
 	CCNode *_catapultArm;
 	CCNode *_levelNode;
 	CCNode *_contentNode;
+	CCNode *_pullbackNode;
 }
 
 // is called when CCB file has completed loading
@@ -22,9 +23,14 @@
 	//Load level 1
 	CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
 	[_levelNode addChild:level];
-	
+
+	// nothing shall collide with our invisible nodes
+	_pullbackNode.physicsBody.collisionMask = @[];
+
 	// visualize physics bodies & joints
 	_physicsNode.debugDraw = TRUE;
+
+	
 }
 
 // called on every touch in this scene
