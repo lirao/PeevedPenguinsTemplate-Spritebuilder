38273fbd0712ab0d2c520ea3cea6cba3cf1bf9ad
Rao Li <rao.li@sv.cmu.edu>
2015-02-08 17:21:42 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -9,92 +9,89 @@
 #import "Gameplay.h"
 
 @implementation Gameplay {
-	CCPhysicsNode *_physicsNode;
-	CCNode *_catapultArm;
-	CCNode *_levelNode;
-	CCNode *_contentNode;
-	CCNode *_pullbackNode;
-	//Mouse pullback
-	CCNode *_mouseJointNode;
-	CCPhysicsJoint *_mouseJoint;
+    CCPhysicsNode* _physicsNode;
+    CCNode* _catapultArm;
+    CCNode* _levelNode;
+    CCNode* _contentNode;
+    CCNode* _pullbackNode;
+    //Mouse pullback
+    CCNode* _mouseJointNode;
+    CCPhysicsJoint* _mouseJoint;
 
-	//Penguin launch
-	CCNode *_currentPenguin;
-	CCPhysicsJoint *_penguinCatapultJoint;
+    //Penguin launch
+    CCNode* _currentPenguin;
+    CCPhysicsJoint* _penguinCatapultJoint;
 }
 
 // is called when CCB file has completed loading
-- (void)didLoadFromCCB {
-	// tell this scene to accept touches
-	self.userInteractionEnabled = TRUE;
-	//Load level 1
-	CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
-	[_levelNode addChild:level];
+- (void)didLoadFromCCB
+{
+    // tell this scene to accept touches
+    self.userInteractionEnabled = TRUE;
+    //Load level 1
+    CCScene* level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 
-	// nothing shall collide with our invisible nodes
-	_pullbackNode.physicsBody.collisionMask = @[];
-	_mouseJointNode.physicsBody.collisionMask = @[];
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
+    _mouseJointNode.physicsBody.collisionMask = @[];
 
-	// visualize physics bodies & joints
-	_physicsNode.debugDraw = TRUE;
+    // visualize physics bodies & joints
+    //_physicsNode.debugDraw = TRUE;
 
-	
+    //become own physics delegate
+    _physicsNode.collisionDelegate = self;
 }
 
 // called on every touch in this scene
-- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+- (void)touchBegan:(CCTouch*)touch withEvent:(CCTouchEvent*)event
+{
 
-	//Get the touchlocation
-	CGPoint touchLocation = [touch locationInNode:_contentNode];
+    //Get the touchlocation
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
 
-	// start catapult dragging when a touch inside of the catapult arm occurs
-	if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
-	{
+    // start catapult dragging when a touch inside of the catapult arm occurs
+    if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation)) {
 
-		// move the mouseJointNode to the touch position
-		_mouseJointNode.position = touchLocation;
+        // move the mouseJointNode to the touch position
+        _mouseJointNode.position = touchLocation;
 
-		// setup a spring joint between the mouseJointNode and the catapultArm
-		_mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:50.f];
+        // setup a spring joint between the mouseJointNode and the catapultArm
+        _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:50.f];
 
-		// create a penguin from the ccb-file
-		_currentPenguin = [CCBReader load:@"Penguin"];
-		// initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
-		CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
-		// transform the world position to the node space to which the penguin will be added (_physicsNode)
-		_currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
-		// add it to the physics world
-		[_physicsNode addChild:_currentPenguin];
-		// we don't want the penguin to rotate in the scoop
-		_currentPenguin.physicsBody.allowsRotation = FALSE;
+        // create a penguin from the ccb-file
+        _currentPenguin = [CCBReader load:@"Penguin"];
+        // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
+        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
+        // transform the world position to the node space to which the penguin will be added (_physicsNode)
+        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
+        // add it to the physics world
+        [_physicsNode addChild:_currentPenguin];
+        // we don't want the penguin to rotate in the scoop
+        _currentPenguin.physicsBody.allowsRotation = FALSE;
 
-		// create a joint to keep the penguin fixed to the scoop until the catapult is released
-		_penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
-
-
-
-	}
+        // create a joint to keep the penguin fixed to the scoop until the catapult is released
+        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
+    }
 }
 
-
-
-- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+- (void)touchMoved:(CCTouch*)touch withEvent:(CCTouchEvent*)event
 {
-	// whenever touches move, update the position of the mouseJointNode to the touch position
-	CGPoint touchLocation = [touch locationInNode:_contentNode];
-	_mouseJointNode.position = touchLocation;
+    // whenever touches move, update the position of the mouseJointNode to the touch position
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
 }
 
--(void) touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+- (void)touchEnded:(CCTouch*)touch withEvent:(CCTouchEvent*)event
 {
-	// when touches end, meaning the user releases their finger, release the catapult
-	[self releaseCatapult];
+    // when touches end, meaning the user releases their finger, release the catapult
+    [self releaseCatapult];
 }
 
--(void) touchCancelled:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+- (void)touchCancelled:(CCTouch*)touch withEvent:(CCTouchEvent*)event
 {
-	// when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
-	[self releaseCatapult];
+    // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
+    [self releaseCatapult];
 }
 
 //- (void)launchPenguin {
@@ -117,28 +114,35 @@
 //	[_contentNode runAction:follow];
 //}
 
-- (void)releaseCatapult {
-	if (_mouseJoint != nil)
-	{
-		// releases the joint and lets the catapult snap back
-		[_mouseJoint invalidate];
-		_mouseJoint = nil;
+- (void)releaseCatapult
+{
+    if (_mouseJoint != nil) {
+        // releases the joint and lets the catapult snap back
+        [_mouseJoint invalidate];
+        _mouseJoint = nil;
 
-		// releases the joint and lets the penguin fly
-		[_penguinCatapultJoint invalidate];
-		_penguinCatapultJoint = nil;
+        // releases the joint and lets the penguin fly
+        [_penguinCatapultJoint invalidate];
+        _penguinCatapultJoint = nil;
 
-		// after snapping rotation is fine
-		_currentPenguin.physicsBody.allowsRotation = TRUE;
+        // after snapping rotation is fine
+        _currentPenguin.physicsBody.allowsRotation = TRUE;
 
-		// follow the flying penguin
-		CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-		[_contentNode runAction:follow];
-	}
+        // follow the flying penguin
+        CCActionFollow* follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:follow];
+    }
 }
 
-- (void)retry {
-	// reload this level
-	[[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+- (void)retry
+{
+    // reload this level
+    [[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"Gameplay"]];
 }
+
+- (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair*)pair seal:(CCNode*)nodeA wildcard:(CCNode*)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
+}
+
 @end
